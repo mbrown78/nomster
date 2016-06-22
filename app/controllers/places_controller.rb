2a0fefedfe5ccbeb75811786ba5b9daf74585800
Melissa Brown <melissabrown851@gmail.com>
2016-06-22 11:24:43 +0000

    added validations to create and update

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -25,15 +25,21 @@ class PlacesController < ApplicationController
     
     def update
       @place = Place.find(params[:id])
-      if @place.user != current_user
-       return render text: 'Not Allowed', status: :forbidden
-      end
+      
+       if @place.user != current_user
+        return render text: 'Not Allowed', status: :forbidden
+       end
+      
       @place.update_attributes(place_params)
-      redirect_to root_path
+        if @place.valid? 
+           redirect_to root_path
+        else render :new, status: :unprocessable_entity
+        end  
     end  
     
     def destroy
       @place = Place.find(params[:id])
+      
       if @place.user != current_user
         return render text: 'Not Allowed', status: :forbidden
       end  
@@ -43,9 +49,13 @@ class PlacesController < ApplicationController
     end  
     
     def create
-    current_user.places.create(place_params)
-    redirect_to root_path
-    end
+    @place = current_user.places.create(place_params)
+    
+      if @place.valid?
+        redirect_to root_path
+      else render :new, status: :unprocessable_entity
+      end
+    end 
 
   private
 
--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,5 @@ class Place < ActiveRecord::Base
      paginates_per 3
      
      belongs_to :user
+     validates :name, presence: true
 end
