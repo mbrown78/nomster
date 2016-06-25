1b422ea44c86fe828afd09516dabba39b62a5035
Melissa Brown <melissabrown851@gmail.com>
2016-06-25 13:28:12 +0000

    fixed geocoder issue

--- config/initializers/gecoder.rb
+++ config/initializers/gecoder.rb
@@ -1,6 +1,6 @@
 
 Geocoder.configure(
     :lookup => :bing, 
-    :api_key => ENV['AgZYJeGA2dufwUH-BAA032bpqGUKYIL-Lm2nOgoDDlBZvdUPzgQsyG-DclabYJRU'],
+    :api_key => ENV['GEOCODER_API_KEY'],
     )
 
