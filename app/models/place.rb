1e2b067e37573be239dfae03c991120b869aabf7
Melissa Brown <melissabrown851@gmail.com>
2016-06-27 09:57:04 +0000

    added comments model

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,12 +2,12 @@ class Place < ActiveRecord::Base
      paginates_per 3
      
      belongs_to :user
+     has_many :comments
      
      geocoded_by :address
      after_validation :geocode
   
      validates :description, :address, presence: true
      validates :name, length: { minimum: 3 }, presence: true
-     
-     
+
 end
